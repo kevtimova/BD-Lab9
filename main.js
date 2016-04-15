ff0efe8c98c1d5dac754d9390ae16254afcf3823
Katrina E <kve216@nyu.edu>
2016-04-15 17:43:14 -0400

    commit

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
+var dataSet=[];
 
-dataSet=[];
-d3.csv("car.csv", function(data) {
+var draw = function(values) {
+
+	xScale.domain(d3.extent(values, function(d) { return d.mpg; }));
+    yScale.domain([0, d3.max(values, function(d) { return d.displacement; })]);
+
+
+	console.log(values);
+	var svg = d3.select('svg')
+	  .attr('width', 400)
+	  .attr('height', 300);
+
+	var circles = svg.selectAll('circle').data(values);
+	
+	circles.enter()
+	.append("circle")
+	  .attr("cy", function(d){return yScale(d.displacement);})
+	  .attr("cx", function(d, i) { return xScale(d.mpg); })
+	  .attr("r", function(d, i) { return 10; })
+	  .attr("alpha",function(d){return.3;});
+	
+
+	//circle.exit().remove();
+};
+
+$(document).ready(function() {
+	
+	d3.csv("car.csv", function(data) {
+
+	xScale = d3.scale.linear().range([400,0]);
+	yScale = d3.scale.linear().range([0,300]);
 
 	data.forEach(function(d){
 		d.mpg = +d.mpg
@@ -14,27 +43,9 @@ d3.csv("car.csv", function(data) {
   console.log(data[0]);
   dataSet=data;
 
+  draw(data);
+
 });
 
-var draw = function(values) {
-
-	var svg = d3.select('svg')
-	  .attr('width', 400)
-	  .attr('height', 300);
-
-	var circle = svg.selectAll("circle")
-      .data(values)
-	.enter()
-	.append("circle")
-	  .attr("cy", 60)
-	  .attr("cx", function(d, i) { return i * 100 + 30; })
-	  .attr("r", function(d) { return d; });
 	
-
-	//circle.exit().remove();
-};
-
-$(document).ready(function() {
-
-	draw([1,2,3,4]);
 });
